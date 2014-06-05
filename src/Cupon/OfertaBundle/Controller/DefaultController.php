fb9d3e0e7740dc2aaef93805743eb81df7a04ce1
davidpablos <davidpablos@gmail.com>
2014-06-05 23:19:54 +0200

    first commit

---
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -0,0 +1,9 @@
+<?php
+namespace Cupon\OfertaBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+class DefaultController extends Controller
+{
+	public function ayudaAction() {
+		return $this->render('OfertaBundle:Default:ayuda.html.twig');
+	}
+}
